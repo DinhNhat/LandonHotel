9a357aaf173db75edac240c694f992d289f87886
LAPTOP-V7CE2C0D\Nhat Dinh <nguyendinhnhat206@gmail.com>
2020-07-22 21:16:46 -0400

    second local commit

--- src/main/java/com/springangular/nhatdinh/rest/ReservationResource.java
+++ src/main/java/com/springangular/nhatdinh/rest/ReservationResource.java
@@ -8,6 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +22,7 @@ import com.springangular.nhatdinh.model.response.ReservationResponse;
 @RequestMapping(ResourceConstants.ROOM_RESERVATION_V1)
 public class ReservationResource {
 	
+	//-------------GET REQUEST---------------//
 	@RequestMapping(path="", method=RequestMethod.GET, produces=APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<ReservationResponse> getAvailableRooms(
 			@RequestParam(value = "checkin") 
@@ -31,6 +33,7 @@ public class ReservationResource {
 		return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
 	}
 	
+	//-------------POST REQUEST---------------//
 	@RequestMapping(path="", method=RequestMethod.POST, 
 			produces=APPLICATION_JSON_UTF8_VALUE, 
 			consumes=APPLICATION_JSON_UTF8_VALUE)
@@ -41,4 +44,21 @@ public class ReservationResource {
 		return new ResponseEntity<>(new ReservationResponse(), HttpStatus.CREATED);
 	}
 	
+	//-------------PUT REQUEST---------------//
+	@RequestMapping(path="", method=RequestMethod.PUT,
+			produces=APPLICATION_JSON_UTF8_VALUE, 
+			consumes=APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<ReservationResponse> updateReservation(
+			@RequestBody
+			ReservationRequest reservationRequest) {
+		
+		return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+	}
+	
+	//-------------DELETE REQUEST---------------//
+	@RequestMapping(path="/{reservationId}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteReservation(@PathVariable long reservationId) {
+		
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 }
